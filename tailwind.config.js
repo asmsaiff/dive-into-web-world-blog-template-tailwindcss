9a575fcafc733570514f2bdb478856153c019d10
saidmsaif <saidmsaiff@gmail.com>
2021-07-28 22:41:10 +0600

    change header bottom latest posts section design from slider to grid layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,13 @@ module.exports = {
         '.container': {
           maxWidth: '100%',
           '@screen sm': {
-            maxWidth: '600px',
-          },
-          '@screen md': {
             maxWidth: '700px',
           },
+          '@screen md': {
+            maxWidth: '900px',
+          },
           '@screen lg': {
-            maxWidth: '800px',
+            maxWidth: '1000px',
           },
           '@screen xl': {
             maxWidth: '1300px',
